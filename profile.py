7ef8a8d12816ac79eaa2ed7345caead8ab60a048
djankowski99 <38013098+djankowski99@users.noreply.github.com>
2018-11-14 16:38:11 -0500

    profile.py
    Profile.py now calls setup.sh. We can put all of our shell command in here.

--- profile.py
+++ profile.py
@@ -15,14 +15,8 @@ request = portal.context.makeRequestRSpec()
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
+node.addService(rspec.Execute(shell ="/bin/sh,
+                              command = "setup.sh")
 
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo suwf allow in "Apache Full"'))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo systemctl status apache2'))
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
