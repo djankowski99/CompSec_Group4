1f858be3bb505265e868609460a340ae1c90312b
djankowski99 <38013098+djankowski99@users.noreply.github.com>
2018-11-14 18:11:37 -0500

    Update profile.py
    Forgot a quote mark.

--- profile.py
+++ profile.py
@@ -1,11 +1,3 @@
-"""An example of constructing a profile with a single Xen VM from CloudLab.
-Instructions:
-Wait for the profile instance to start, and then log in to the VM via the
-ssh port specified below.  (Note that in this case, you will need to access
-the VM through a high port on the physical host, since we have not requested
-a public IP address for the VM itself.)
-"""
-
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
@@ -15,7 +7,7 @@ request = portal.context.makeRequestRSpec()
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
-node.addService(rspec.Execute(shell ="/bin/sh,
+node.addService(rspec.Execute(shell ="sh",
                               command = "setup.sh")
 
 # Print the RSpec to the enclosing page.
