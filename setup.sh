bd8691c707ca04df9042f346a1b3c39a5596fa6c
djankowski99 <38013098+djankowski99@users.noreply.github.com>
2018-11-14 18:08:59 -0500

    Update setup.sh
    Changed some stuff

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+set -x
 sudo apt -y update
+
+# create a user named seed with password dees. 
+sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
+sudo usermod -a -G sudo seed
+
+
 #setup apache
 sudo apt install -y apache2
 sudo ufw allow in "ApacheFull"
@@ -6,18 +13,14 @@ sudo systemct1 enable apache2
 
 #setup anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
-sudo bash -c -y "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
-sudo bash -c -y "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
-sudo bash -c -y "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
+sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
+sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
 # allow ports
 setup ufw allow 8888
 setup ufw allow 22
 sudo ufw enable
-
-# create a user named seed with password dees. 
-sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
-sudo usermod -a -G sudo seed
 
 #setup jupter
 jupyter notebook --generate-config
