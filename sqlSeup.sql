ef090664033f5653f942fda222a193d10082ca07
djankowski99 <38013098+djankowski99@users.noreply.github.com>
2018-11-29 14:30:21 -0500

    Update sqlSeup.sql
    Granted the elgg_admin user privalges

--- sqlSeup.sql
+++ sqlSeup.sql
@@ -1,2 +1,3 @@
 UPDATE mysql.user SET Password = PASSWORD('password') WHERE User = 'root';
 CREATE USER 'elgg_admin'@'localhost' IDENTIFIED BY 'seedubuntu';
+GRANT ALL PRIVILEGES ON *.* TO 'elgg_admin'@'localhost' IDENTIFIED BY 'seedubuntu';
