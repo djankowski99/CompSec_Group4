4ee755d564e6da40e873165eee38d22dbf05eb8b
NBluder233 <41296084+NBluder233@users.noreply.github.com>
2018-11-29 14:09:50 -0500

    Added lab 1 source codes

---
+++ prog2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+int main()
+{
+  char *argv[2];
+  argv[0] = "/usr/bin/env";
+  argv[1] = NULL;
+  execve("/usr/bin/env", argv, NULL); //Line 1
+  return 0 ;
+}
