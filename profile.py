6ae65c37406e85bcaa8236a3e5d8033910735ff8
djankowski99 <38013098+djankowski99@users.noreply.github.com>
2018-12-04 14:56:04 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -17,7 +17,7 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo bash /local/repository/setup_files/setup.sh"))
+                              command="sudo bash /local/repository/setup.sh"))
 
 #node.addService(rspec.Execute(shell="/bin/sh",
 #                              command="sudo apt update"))
