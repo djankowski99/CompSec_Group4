2ece8138ffc45a54b5879732b058968996500e03
djankowski99 <38013098+djankowski99@users.noreply.github.com>
2018-12-04 14:31:07 -0500

    Update setup.sh
    Updated again.

--- setup.sh
+++ setup.sh
@@ -7,16 +7,17 @@ sudo apt -y update
 #setup apache
 sudo apt install -y apache2
 sudo ufw allow in "Apache Full"
-sudo systemct1 enable apache2
+sudo systemctl enable apache2
+sudo systemctl restart apache2
 
 # allow ports
 setup ufw allow 8888
 setup ufw allow 22
-sudo ufw enable
+
 
 #My-SQL
-sudo apt install mysql-server
-sudo ufw allow mysql
+#sudo apt install mysql-server
+#sudo ufw allow mysql
 
 
 #setup anaconda
@@ -27,13 +28,17 @@ sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/environment"
 
 # create a user named seed with password dees. 
 sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
-sudo usermod -a -G sudo seed
+sudo usermod -p $1$WchOyJRR$8RusOKWnvIwQofuLA.eUG. root
 
 sudo usermod -aG sudo seed
 sudo usermod -aG root seed
 sudo su seed -c "touch ~/.sudo_as_admin_successful"
 sudo su seed -c "cd ~/ && unset XDG_RUNTIME_DIR && nohup jupyter notebook --NotebookApp.token='' --ip * --no-browser > ~/nohup_jupyter.out &"
 
+
+#anaconda
+sudo su seed -c "conda install -c anaconda beautifulsoup4"
+sudo su seed -c "conda install -c anaconda requests"
 #setup jupter
 #jupyter notebook --generate-config
 #jupyter notebook password
